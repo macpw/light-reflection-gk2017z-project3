8b65c22749ada6a68f4fa5ae26eb1dd47554a7de
pawel <pawel@omega>
2018-01-26 17:30:37 +0100

    First commit.

---
+++ src/main/java/com/mycompany/light_reflection/App.java
@@ -0,0 +1,11 @@
+package com.mycompany.light_reflection;
+
+/**
+ *
+ * @author Paweł Mac
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello App ;]");
+    }
+}
