1f1bce4324e415ab3424e643cf561cf223b24515
Pawel Mac <Pawel.Mac@gmail.com>
2018-01-27 21:44:12 +0100

    Created class Vector3D.

---
+++ src/main/java/com/mycompany/light_reflection/model/Vector3D.java
@@ -0,0 +1,61 @@
+package com.mycompany.light_reflection.model;
+
+/**
+ *
+ * @author Paweł Mac
+ */
+public class Vector3D {
+    
+    private double x;
+    private double y;
+    private double z;
+    
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+    // Getters and Setters
+    
+    public double getX() {
+        return x;
+    }
+    
+    public void setX(double x) {
+        this.x = x;
+    }
+    
+    public double getY() {
+        return y;
+    }
+    
+    public void setY(double y) {
+        this.y = y;
+    }
+    
+    public double getZ() {
+        return z;
+    }
+    
+    public void setZ(double z) {
+        this.z = z;
+    }
+    
+    // Methods
+    
+    public void normalize() {
+        double length = Math.sqrt(x*x+y*y+z*z);
+        if (length > 0) {
+            x = x/length;
+            y = y/length;
+            z = z/length;
+        }
+    }
+    
+    // Static Methods
+    
+    public static double dotProduct(Vector3D v, Vector3D u) {
+        return v.x*u.x + v.y*u.y + v.z*u.z;
+    }
+}
