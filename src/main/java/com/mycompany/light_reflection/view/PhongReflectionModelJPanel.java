c5ccd5c4c9e9f79ae13fa5ab173876bc37a80663
Pawel Mac <Pawel.Mac@gmail.com>
2018-01-29 10:51:27 +0100

    Updated PhongReflectionModelJPanel. Added IntegerDocumentFilter and FloatingPointDocumentFilter.

--- src/main/java/com/mycompany/light_reflection/view/PhongReflectionModelJPanel.java
+++ src/main/java/com/mycompany/light_reflection/view/PhongReflectionModelJPanel.java
@@ -1,14 +1,19 @@
 package com.mycompany.light_reflection.view;
 
+import com.mycompany.light_reflection.view.util.FloatingPointDocumentFilter;
+import com.mycompany.light_reflection.view.util.IntegerDocumentFilter;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.Document;
 
 /**
  *
@@ -16,14 +21,14 @@ import javax.swing.border.TitledBorder;
  */
 public final class PhongReflectionModelJPanel extends JPanel {
     
-    JLabel formulaJLabel = new JLabel(
+    private final JLabel formulaJLabel = new JLabel(
         "<html>"+
             "I = I<sub>a</sub>k<sub>a</sub> + "+
                 "f<sub>att</sub>I<sub>p</sub>k<sub>d</sub>(N◦L) + "+
                 "f<sub>att</sub>I<sub>p</sub>k<sub>s</sub>cos<sup>n</sup>α"+
         "</html>"
     );
-    JLabel descriptionJLabel = new JLabel(
+    private final JLabel descriptionJLabel = new JLabel(
         "<html>"+
             "<p>I<sub>a</sub> - ambient lighting intensity</p>"+
             "<p>I<sub>p</sub> - intensity for a point light source</p>"+
@@ -41,22 +46,26 @@ public final class PhongReflectionModelJPanel extends JPanel {
             "</html>"
     );
     
-    JLabel I_aJLabel = new JLabel("<html>I<sub>a</sub>=</html>");
-    JLabel I_pJLabel = new JLabel("<html>I<sub>p</sub>=</html>");
-    JTextField I_aJTextField = new JTextField(1);
-    JTextField I_pJTextField = new JTextField(1);
+    private final JPanel descriptionJPanel = new JPanel(new GridBagLayout());
     
-    JLabel k_aJLabel = new JLabel("<html>k<sub>a</sub>=</html>");
-    JLabel k_dJLabel = new JLabel("<html>k<sub>d</sub>=</html>");
-    JLabel k_sJLabel = new JLabel("<html>k<sub>s</sub>=</html>");
-    JTextField k_aJTextField = new JTextField(1);
-    JTextField k_dJTextField = new JTextField(1);
-    JTextField k_sJTextField = new JTextField(1);
+    private final JButton descriptionJButton = new JButton("show description");
     
-    JLabel nJLabel = new JLabel("n=");
-    JTextField nJTextField = new JTextField(1);
-    JLabel cJLabel = new JLabel("c=");
-    JTextField cJTextField = new JTextField(1);
+    private final JLabel I_aJLabel = new JLabel("<html>I<sub>a</sub>=</html>");
+    private final JLabel I_pJLabel = new JLabel("<html>I<sub>p</sub>=</html>");
+    private final JTextField I_aJTextField = new JTextField(1);
+    private final JTextField I_pJTextField = new JTextField(1);
+    
+    private final JLabel k_aJLabel = new JLabel("<html>k<sub>a</sub>=</html>");
+    private final JLabel k_dJLabel = new JLabel("<html>k<sub>d</sub>=</html>");
+    private final JLabel k_sJLabel = new JLabel("<html>k<sub>s</sub>=</html>");
+    private final JTextField k_aJTextField = new JTextField(1);
+    private final JTextField k_dJTextField = new JTextField(1);
+    private final JTextField k_sJTextField = new JTextField(1);
+    
+    private final JLabel nJLabel = new JLabel("n=");
+    private final JTextField nJTextField = new JTextField(1);
+    private final JLabel cJLabel = new JLabel("c=");
+    private final JTextField cJTextField = new JTextField(1);
     
     public PhongReflectionModelJPanel() {
         formulaJLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 14));
@@ -69,6 +78,24 @@ public final class PhongReflectionModelJPanel extends JPanel {
         k_sJTextField.setHorizontalAlignment(JTextField.CENTER);
         nJTextField.setHorizontalAlignment(JTextField.CENTER);
         cJTextField.setHorizontalAlignment(JTextField.CENTER);
+        
+        IntegerDocumentFilter integerDocumentFilter = new IntegerDocumentFilter();
+        FloatingPointDocumentFilter floatingPointDocumentFilter = new FloatingPointDocumentFilter();
+        
+        Document I_aJTextFieldDocument = I_aJTextField.getDocument();
+        ((AbstractDocument) I_aJTextFieldDocument).setDocumentFilter(integerDocumentFilter);
+        Document I_pJTextFieldDocument = I_pJTextField.getDocument();
+        ((AbstractDocument) I_pJTextFieldDocument).setDocumentFilter(integerDocumentFilter);
+        Document k_aJTextFieldDocument = k_aJTextField.getDocument();
+        ((AbstractDocument) k_aJTextFieldDocument).setDocumentFilter(floatingPointDocumentFilter);
+        Document k_dJTextFieldDocument = k_dJTextField.getDocument();
+        ((AbstractDocument) k_dJTextFieldDocument).setDocumentFilter(floatingPointDocumentFilter);
+        Document k_sJTextFieldDocument = k_sJTextField.getDocument();
+        ((AbstractDocument) k_sJTextFieldDocument).setDocumentFilter(floatingPointDocumentFilter);
+        Document nJTextFieldDocument = nJTextField.getDocument();
+        ((AbstractDocument) nJTextFieldDocument).setDocumentFilter(integerDocumentFilter);
+        Document cJTextFieldDocument = cJTextField.getDocument();
+        ((AbstractDocument) cJTextFieldDocument).setDocumentFilter(floatingPointDocumentFilter);
         
         GridBagConstraints gbc = new GridBagConstraints();
         
@@ -134,9 +161,9 @@ public final class PhongReflectionModelJPanel extends JPanel {
         gbc.gridy++;
         ncJPanel.add(cJTextField, gbc);
         
-        JPanel descriptionJPanel = new JPanel(new GridBagLayout());
         descriptionJPanel.setBorder(new TitledBorder("description"));
         descriptionJPanel.add(descriptionJLabel);
+        descriptionJPanel.setVisible(false);
         
         this.setBorder(new TitledBorder("Phong Model"));
         this.setLayout(new GridBagLayout());
@@ -152,7 +179,7 @@ public final class PhongReflectionModelJPanel extends JPanel {
         this.add(formulaJLabel, gbc);
         gbc.gridy++;
         gbc.gridwidth = 1;// default
-        gbc.fill = GridBagConstraints.BOTH;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.weightx = 1.0d;
         gbc.weighty = 1.0d;
         this.add(luminousIntensityJPanel, gbc);
@@ -166,7 +193,51 @@ public final class PhongReflectionModelJPanel extends JPanel {
         gbc.gridy++;
         gbc.gridx = 0;
         gbc.gridwidth = 3;
+        this.add(descriptionJButton, gbc);
+        gbc.gridy++;
         this.add(descriptionJPanel, gbc);
+    }
+    
+    // Getters
+    
+    public JLabel getDescriptionJLabel() {    
+        return descriptionJLabel;
+    }
+    
+    public JPanel getDescriptionJPanel() {
+        return descriptionJPanel;
+    }
+    
+    public JButton getDescriptionJButton() {
+        return descriptionJButton;
+    }
+    
+    public JTextField getI_aJTextField() {
+        return I_aJTextField;
+    }
+    
+    public JTextField getI_pJTextField() {
+        return I_pJTextField;
+    }
+    
+    public JTextField getK_aJTextField() {
+        return k_aJTextField;
+    }
+    
+    public JTextField getK_dJTextField() {
+        return k_dJTextField;
+    }
+    
+    public JTextField getK_sJTextField() {
+        return k_sJTextField;
+    }
+    
+    public JTextField getnJTextField() {
+        return nJTextField;
+    }
+    
+    public JTextField getcJTextField() {    
+        return cJTextField;
     }
     
     // Test PhongReflectionModelJPanel
