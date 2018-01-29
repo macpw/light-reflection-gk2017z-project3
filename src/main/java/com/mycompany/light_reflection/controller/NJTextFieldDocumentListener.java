0f46a644b88f3783912bc64458b14047817f0f2b
Pawel Mac <Pawel.Mac@gmail.com>
2018-01-29 07:43:58 +0100

    Created class NJTextFieldDocumentListener.

---
+++ src/main/java/com/mycompany/light_reflection/controller/NJTextFieldDocumentListener.java
@@ -0,0 +1,27 @@
+package com.mycompany.light_reflection.controller;
+
+import com.mycompany.light_reflection.model.ViewportModel;
+import java.awt.Color;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Paweł Mac
+ */
+public class NJTextFieldDocumentListener extends AbstractJTextFieldDocumentListener {
+    
+    public NJTextFieldDocumentListener(ViewportModel viewportModel, JTextField jTextField) {
+        super(viewportModel, jTextField);
+    }
+    
+    @Override
+    protected void doUpdate() {
+        try {
+            double parsedDouble = Double.parseDouble(jTextField.getText());
+            viewportModel.setN(parsedDouble);
+            jTextField.setToolTipText("n="+viewportModel.getN());
+        } catch (NumberFormatException nfe) {
+            jTextField.setBackground(Color.orange);
+        }
+    }
+}
