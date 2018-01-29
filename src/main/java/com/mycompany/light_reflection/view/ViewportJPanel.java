875d63313cfc4f14bfa01cbd9a86532a1df60772
Pawel Mac <Pawel.Mac@gmail.com>
2018-01-29 12:32:21 +0100

    Updated ViewportJPanel. Added method paintComponent(Graphics g).

--- src/main/java/com/mycompany/light_reflection/view/ViewportJPanel.java
+++ src/main/java/com/mycompany/light_reflection/view/ViewportJPanel.java
@@ -1,7 +1,15 @@
 package com.mycompany.light_reflection.view;
 
+import com.mycompany.light_reflection.model.Point3D;
+import com.mycompany.light_reflection.model.Sphere;
+import com.mycompany.light_reflection.model.Vector3D;
+import com.mycompany.light_reflection.model.ViewportModel;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.JPanel;
@@ -12,9 +20,104 @@ import javax.swing.JPanel;
  */
 public final class ViewportJPanel extends JPanel implements Observer {
     
+    ViewportModel viewportModel;
+    
     public ViewportJPanel(int width, int height) {
         this.setPreferredSize(new Dimension(width, height));
         this.setBackground(Color.white);
+        this.setBackground(Color.black);
+    }
+    
+    // Setters
+    
+    public void setViewportModel(ViewportModel viewportModel) {
+        this.viewportModel = viewportModel;
+    }
+    
+    // Methods
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2D = (Graphics2D) g;
+        
+        if (viewportModel != null) {
+            Sphere sphere = viewportModel.getSphere();
+            Point3D center = sphere.getCenter();
+            Point3D light = viewportModel.getLight();
+            double focalDistance = viewportModel.getFocalDistance();
+            int viewportWidth = viewportModel.getViewportWidth();
+            int viewportHeight = viewportModel.getViewportHeight();
+            int xShift = viewportWidth/2;
+            int yShift = viewportHeight/2;
+            
+            double I_a = viewportModel.getI_a();
+            double I_p = viewportModel.getI_p();
+            double n = viewportModel.getN();
+            double k_a = viewportModel.getK_a();
+            double k_d = viewportModel.getK_d();
+            double k_s = viewportModel.getK_s();
+            double c = viewportModel.getC();
+            
+            // reduce iterations number
+            int limit = (int) sphere.getR();
+            limit++;
+            yShift = limit;
+            xShift = limit;
+            
+            for (int y = yShift; y > -yShift; y--) {
+                for (int x = -xShift; x < xShift; x++) {
+                    Point3D[] intersectionWithLine = sphere.getIntersectionWithLine(x, y);
+                    if (intersectionWithLine != null) {
+                        Point3D p1 = intersectionWithLine[0];
+                        Point3D p2 = intersectionWithLine[1];
+                        Point3D point3D = p1.isInFrontOfViewport(focalDistance) ? p1 : p2;
+                        point3D.calculatePoint2D(focalDistance, viewportWidth, viewportHeight);
+                        Point2D point2D = point3D.getPoint2D();
+                        Color color = point3D.equals(p1) ? sphere.getExternalColor() : sphere.getInternalColor();
+                        
+                        // N -- vector normal to the surface
+                        Vector3D N = new Vector3D(point3D.getX() - center.getX(), point3D.getY() - center.getY(), point3D.getZ() - center.getZ());
+                        N.normalize();
+                        // V -- vector directed to the observer
+                        Vector3D V = new Vector3D(-point3D.getX(), -point3D.getY(), -point3D.getZ());
+                        V.normalize();
+                        // L -- vector directed to the light source
+                        Vector3D L = new Vector3D(light.getX() - point3D.getX(), light.getY() - point3D.getY(), light.getZ() - point3D.getZ());;
+                        L.normalize();
+                        
+                        double NLdotProduct = Vector3D.dotProduct(N, L);
+                        
+                        // R -- reflection from L
+                        // R = 2(N◦L)N - L
+                        Vector3D R = new Vector3D(2*NLdotProduct*N.getX() - L.getX(), 2*NLdotProduct*N.getY() - L.getY(), 2*NLdotProduct*N.getZ() - L.getZ());
+                        R.normalize();
+                        
+                        // cos(α) = (v◦u) / (|v||u|)
+                        double cos_alpha = Vector3D.dotProduct(V, R);
+                        
+                        double f_att = 1.0 / (c+L.length());
+                        
+                        // I = Iₐ∙kₐ + fₐₜₜ∙Iₚ∙kd∙(N◦L) + fₐₜₜ∙Iₚ∙kₛ∙cosⁿα
+                        // I = Iₐ∙kₐ + fₐₜₜ∙Iₚ∙kd∙(N◦L) + fₐₜₜ∙Iₚ∙kₛ∙(V◦R)ⁿ
+                        // I = Iₐ∙kₐ + fₐₜₜ∙Iₚ∙(kd∙(N◦L) + kₛ∙cosⁿα)
+                        double I = I_a*k_a + f_att*I_p*(k_d*NLdotProduct + k_s*Math.pow(cos_alpha, n));
+                        
+                        I = Math.min(Math.max(I, 0), 255);
+                        
+                        int red   = (int) (I*color.getRed()/255.0);
+                        int green = (int) (I*color.getGreen()/255.0);
+                        int blue  = (int) (I*color.getBlue()/255.0);
+                        
+                        color = new Color(red, green, blue);
+                        
+                        g2D.setPaint(color);
+                        g2D.draw(new Line2D.Double(point2D, point2D));
+                    }
+                }
+            }
+        } else {
+            System.out.println("viewportModel == null");
+        }
     }
     
     @Override
