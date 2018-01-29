6eadae62e515125dabe7da0162f80d7a8e77a949
Pawel Mac <Pawel.Mac@gmail.com>
2018-01-29 07:14:24 +0100

    Created class I_pJTextFieldDocumentListener.

---
+++ src/main/java/com/mycompany/light_reflection/controller/I_pJTextFieldDocumentListener.java
@@ -0,0 +1,27 @@
+package com.mycompany.light_reflection.controller;
+
+import com.mycompany.light_reflection.model.ViewportModel;
+import java.awt.Color;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Paweł Mac
+ */
+public class I_pJTextFieldDocumentListener extends AbstractJTextFieldDocumentListener {
+    
+    public I_pJTextFieldDocumentListener(ViewportModel viewportModel, JTextField jTextField) {
+        super(viewportModel, jTextField);
+    }
+    
+    @Override
+    protected void doUpdate() {
+        try {
+            double d = Double.parseDouble(jTextField.getText());
+            viewportModel.setI_p(d);
+            jTextField.setToolTipText("<html>I<sub>p</sub>="+viewportModel.getI_p()+"</html>");
+        } catch (NumberFormatException nfe) {
+            jTextField.setBackground(Color.orange);
+        }
+    }
+}
